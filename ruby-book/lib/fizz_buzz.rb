f021c9202f5691b2973dd56691c911175f1e89c8
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-09 18:51:50 +0900

    Add test for fizz_buzz.rb

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -1,19 +1,11 @@
 def fizz_buzz(n)
   if n % 15 == 0
-    'fizz buzz'
+    'Fizz Buzz'
   elsif n % 3 == 0
-    'fizz'
+    'Fizz'
   elsif n % 5 == 0
-    'buzz'
+    'Buzz'
   else
     n.to_s
   end
 end
-
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(15)
