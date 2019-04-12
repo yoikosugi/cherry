6dd8a03a006674bb25303dff39810cb41f54cf18
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-12 19:53:25 +0900

    ハッシュ

---
+++ section5/52.rb
@@ -0,0 +1,21 @@
+a = {}.class
+p a
+
+a = { 'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee' }
+p a
+
+{
+  'japan' => 'yen',
+  'us' => 'dollar',
+  'india' => 'rupee'
+}
+
+{
+  'japan' => 'yen',
+  'us' => 'dollar',
+  'india' => 'rupee',
+}
+
+a = { 'japan' => 'yen', 'japan' => '円' }
+p a
+
