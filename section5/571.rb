7f5d33f199a34ea1900173b77aa1f34634377403
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-13 01:04:51 +0900

    シンボルを作成するさまざまな方法

---
+++ section5/571.rb
@@ -0,0 +1,26 @@
+:apple
+:Apple
+:ruby_is_fun
+:okay?
+:welcom!
+:_secret
+:$dollar
+:@at_mark
+
+# 無効
+# :12345
+# :ruby-is-fun
+# :ruby is fun
+# :()
+
+:'12345'
+:'ruby-is-fun'
+:'ruby is fun'
+:'()'
+
+name = 'Alice'
+:"#{name.upcase!}"
+
+hash = { 'abc': 123 }
+
+
