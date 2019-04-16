0e0794a6892c10bce7ebbf2adabd6e982eb52e19
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-16 19:07:11 +0900

    クラスメソッドをインスタンスメソッドで呼び出す

---
+++ section7/753.rb
@@ -0,0 +1,24 @@
+class Product
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def self.format_price(price)
+    "#{price}円"
+  end
+
+  def to_s
+    formatted_price = Product.format_price(price)
+    "name: #{name}, price: #{formatted_price}"
+  end
+end
+
+product = Product.new('A great movie', 1000)
+p product.to_s
+
+p Product.format_price(1000)
+
+p Product.class
