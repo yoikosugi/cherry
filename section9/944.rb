6af8688609df7cab56d2ba0426d4aa9dce07cbc7
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-23 17:47:13 +0900

    例外処理よりも条件分岐を使う

---
+++ section9/944.rb
@@ -0,0 +1,18 @@
+require 'date'
+
+def convert_heisei_to_date(heisei_text)
+  m = heisei_text.match(/平成(?<jp_year>\d+)年(?<month>\d+)月(?<day>\d+)日/)
+  year = m[:jp_year].to_i + 1988
+  month = m[:month].to_i
+  day = m[:day].to_i
+  # 例外処理の範囲を狭め、補足する例外クラスを限定する
+  begin
+    Date.new(year, month, day)
+  rescue ArgumentError
+    # 無効な日付であればnilを返す
+    nil
+  end
+end
+
+p convert_heisei_to_date('平成28年12月31日')
+p convert_heisei_to_date('平成28年99月99日')
