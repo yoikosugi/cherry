214a1a782ee0b957ec0e67d33c5820a0b89dec01
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-24 00:22:30 +0900

    ensureの代わりにブロックを使う

---
+++ section9/962.rb
@@ -0,0 +1,11 @@
+# ブロック付きでオープンすると、メソッドの実行後に自動的にクローズされる
+File.open('some.txt', 'w') do |file|
+  file << 'Hello'
+end
+
+file.open('some.txt', 'w') do |file|
+  file << 'Hello'
+  # わざと例外を発生させる
+  1 / 0
+end
+# 例外は発生するものの、openメソッドによってクローズ処理自体は必ず行われる
