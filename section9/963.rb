29949311011f5599281f475a64750c8e66ef426c
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-24 00:36:06 +0900

    例外が発生しなかった場合のelse

---
+++ section9/963.rb
@@ -0,0 +1,26 @@
+# begin
+#   # 例外が発生するかもしれない処理
+# rescue
+#   # 例外が発生した場合の処理
+# else
+#   # 例外が発生しなかった場合の処理
+# ensure
+#   # 例外の有無に関わらず実行する処理
+# end
+
+# else節を使う場合
+begin
+  puts 'Hello.'
+rescue
+  puts '例外が発生しました。'
+else
+  puts '例外は発生しませんでした。'
+end
+
+# else節を使わない場合
+begin
+  puts 'Hello.'
+  puts '例外は発生しませんでした。'
+rescue
+  puts '例外が発生しました。'
+end
