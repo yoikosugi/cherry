7d647dd8a488f927f9b3206050a1c0b772994a47
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-24 11:18:44 +0900

    例外処理のbegin/endを省略できるケース

---
+++ section9/967.rb
@@ -0,0 +1,31 @@
+# def fizz_buzz(n)
+#   begin
+#     if n % 15 == 0
+#       'Fizz Buzz'
+#     elsif n % 3 == 0
+#       'Fizz'
+#     elsif n % 5 == 0
+#       'Buzz'
+#     else
+#       n.to_s
+#     end
+#   rescue => e
+#     puts "#{e.class} #{e.message}"
+#   end
+# end
+# fizz_buzz(nil)
+
+def fizz_buzz(n)
+  if n % 15 == 0
+    'Fizz Buzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
+rescue => e
+  puts "#{e.class} #{e.message}"
+end
+fizz_buzz(nil)
