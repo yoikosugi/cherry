3fc276e99807bea129758585261ce087ec3ec0d8
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-24 15:17:44 +0900

    rescueした例外を再度発生させる

---
+++ section9/968.rb
@@ -0,0 +1,18 @@
+def fizz_buzz(n)
+  if n % 15 == 0
+    'Fizz Buzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
+rescue => e
+  # 発生した例外をログやメールに残す
+  puts "[LOG] エラーが発生しました: #{e.class} #{e.message}"
+  # 捕捉した例外を再度発生させ、プログラム自体は異常終了させる
+  raise
+end
+
+fizz_buzz(nil)
