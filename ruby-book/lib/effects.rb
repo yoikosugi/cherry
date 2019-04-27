03cade0ca0b29604ce999e2dbb65928631ab3fae
yoshihiko sughimura <yoikosugi@gmail.com>
2019-04-27 17:01:19 +0900

    word_synth/worf_synth_test

---
+++ ruby-book/lib/effects.rb
@@ -0,0 +1,23 @@
+# Effectsはクラスメソッドを提供するだけで、自分のインスタンスを作成する要件はないのでmodule
+module Effects
+  def self.reverse
+    ->(words) do
+      words.split(' ').map(&:reverse).join(' ')
+    end
+  end
+
+  def self.echo(rate)
+    ->(words) do
+      # スペースならそのまま返す
+      # スペース以外ならその文字を指定された回数だけ繰り返す
+      words.chars.map { |c| c == ' ' ? c : c * rate }.join
+    end
+  end
+
+  def self.loud(level)
+    ->(words) do
+      # スペースで分割 > 大文字変換と"!"の付与 > スペースで連結
+      words.split(' ').map { |word| word.upcase + '!' * level }.join(' ')
+    end
+  end
+end
